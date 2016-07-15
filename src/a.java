c1c53277c9af8c25e4daae782d9624d5ad27e4e7
xuzhiyou <xuzhiyou0223@163.com>
2016-07-15 13:38:46 +0800

    changes log

--- src/a.java
+++ src/a.java
@@ -3,6 +3,7 @@ public class a {
 	public static void main(String[]args){
 		System.out.println("hello good boy");
 		System.out.print("hello beautiful girl");
+		System.out.print("hello everyone");
 		
 	}
 
